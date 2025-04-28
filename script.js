371366248afc558e44b25c497c97a2f0cdff0945
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-04-29 01:19:57 +0400

    Commit

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 let value = document.querySelector("h2")
 let input = document.getElementById("input")
-input.addEventListener("input", () => {
-    if (input.value <= 9) {
-        value.innerHTML = `$0${input.value}.00 <span>/ month</span>`
+
+let check = document.getElementById("check")
+
+let h4 = document.querySelector("h4")
+
+check.addEventListener("input", () => {
+    if (check.checked) {
+        value.innerHTML = `$${15 * 12}.00 <span>/ year</span>`
     } else {
-        value.innerHTML = `$${input.value}.00 <span>/ month</span>`
+        value.innerHTML = `$${15}.00 <span>/ month</span>`
     }
+})
+
+
+input.addEventListener("input", () => {
+    if (check.checked) {
+        value.innerHTML = `$${input.value * 12}.00 <span>/ year</span>`
+    } else {
+        if (input.value <= 9) {
+            value.innerHTML = `$0${input.value}.00 <span>/ month</span>`
+        } else {
+            value.innerHTML = `$${input.value}.00 <span>/ month</span>`
+        }
+    }
+
+    h4.textContent = `${Math.ceil((100 / 15) * input.value)}K PAGEVIEWS`
 })
 
 let viewBill = document.querySelector(".views-bill")
